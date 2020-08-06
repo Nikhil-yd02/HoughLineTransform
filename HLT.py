d8d23c544288216e109fe007703fb7d81d4d6a43
NIKHIL YADAV <58444677+Nikhil-yd02@users.noreply.github.com>
2020-08-06 23:14:43 +0530

    Add files via upload

--- HLT.py
+++ HLT.py
@@ -5,12 +5,12 @@ from matplotlib import pyplot as plt
 
 def hough_line(img):
 
-    # creating theta, cos_theta, sin_theta:
-    theta = np.deg2rad(np.arange(-90, 90))
-    cos_theta = np.cos(theta)
-    sin_theta = np.sin(theta)
+    # creating thetas, cos_thetas, sin_thetas:
+    thetas = np.deg2rad(np.arange(-90, 90))
+    cos_thetas = np.cos(thetas)
+    sin_thetas = np.sin(thetas)
     # length of theta:
-    num_theta = len(theta)
+    num_theta = len(thetas)
 
     # creating rhos:
     width, height = img.shape
@@ -26,30 +26,56 @@ def hough_line(img):
     for i in range(len(x_indexes)):
         for t_index in range(num_theta):
             # Calculating rho & diagonal_len is added for a positive index value:
-            rho = int(round(x_indexes[i] * cos_theta[t_index] + y_indexes[i] * sin_theta[t_index])) + diagonal_len
+            rho = int(round(x_indexes[i] * cos_thetas[t_index] + y_indexes[i] * sin_thetas[t_index])) + diagonal_len
             accumulator[rho, t_index] += 1
 
-    return accumulator, theta, rhos
+    return accumulator, thetas, rhos
 
 
-# creating a gray scale image:
-img = 255*np.ones((256, 256), dtype=np.uint8)
-cv2.rectangle(img, (100, 100), (200, 200), 1, -1)
+# creating a image:
+img = np.zeros((256, 256), dtype=np.uint8)
+cv2.line(img, (100, 100), (200, 200), (255, 255, 0))
 
 # applying canny edge detection:
 edges = cv2.Canny(img, 100, 200)
 
-# calling hough_line transform function:
-accumulator, theta, rhos = hough_line(img)
-
-# Subplotting image, Canny edge, accumulator:
-plt.subplot(221)
+# Sub-plotting image, Canny edge:
+plt.subplot(121)
 plt.imshow(img, cmap='gray', interpolation='nearest')
 plt.title('Original')
-plt.subplot(222)
+plt.subplot(122)
 plt.imshow(edges, cmap='gray', interpolation='nearest')
 plt.title('Canny edges')
-plt.subplot(223)
-plt.imshow(accumulator, cmap='jet', interpolation='nearest')
+plt.show()
+
+# calling hough_line transform function:
+accumulator, thetas, rhos = hough_line(img)
+
+# peak finding based on max votes:
+mav_votes = np.argmax(accumulator)
+print('mav_votes =', mav_votes)
+rho = rhos[int(mav_votes/accumulator.shape[1])]
+theta = thetas[mav_votes % accumulator.shape[1]]
+print('rho =', rho, ', theta =', np.rad2deg(theta))
+
+# getting coordinates of the line we want to draw/project:
+a = np.cos(theta)
+b = np.sin(theta)
+x0 = a*rho
+y0 = b*rho
+x1 = int(x0 + 1000*(-b))
+y1 = int(y0 + 1000*a)
+x2 = int(x0 - 1000*(-b))
+y2 = int(y0 - 1000*a)
+
+# getting the result:
+cv2.line(img, (x1, y1), (x2, y2), 255)
+
+# Sub-plotting accumulator, result:
+plt.subplot(121)
+plt.imshow(accumulator, origin='lower')
 plt.title('Accumulator')
+plt.subplot(122)
+plt.imshow(img, cmap='gray', interpolation='nearest')
+plt.title('result')
 plt.show()
